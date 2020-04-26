197825d51e0e25a6be5e8ed6ca8fcf7a2697ad62
Daniel <dvasilev@axmit.com>
2020-04-26 12:52:20 +0300

    FE[#HCN-14] FEATURE: Add applications transport

--- src/entities/Application/Application.transport.ts
+++ src/entities/Application/Application.transport.ts
@@ -2,11 +2,21 @@ import axios from 'axios';
 import { objectToQuery } from 'common/helpers/filters.helper';
 import { IUpdateWorkspaceModelTo, IWorkspaceModelTo, IWorkspaceCollectionParams } from './Application.models';
 
-const basePath = '/workspaces';
+const basePathApplications = '/applications';
+const basePathWorkspaces = '/workspaces';
 
 export const applicationTransport = {
-  addWorkspace: (model: IWorkspaceModelTo) => axios.post(`${basePath}`, model).then(r => r.data),
-  getWorkspace: (id: string) => axios.get(`${basePath}/${id}`).then(r => r.data),
-  getWorkspaces: (params: IWorkspaceCollectionParams) => axios.get(`${basePath}${objectToQuery(params)}`).then(r => r.data),
-  updateWorkspace: (params: IUpdateWorkspaceModelTo) => axios.patch(`${basePath}/${params.id}/${params.action}`, { ...params })
+  addApplication: (model: IWorkspaceModelTo) => axios.post(`${basePathApplications}`, model).then(r => r.data),
+  getApplication: (id: string) => axios.get(`${basePathApplications}/${id}`).then(r => r.data),
+  getApplications: (params: IWorkspaceCollectionParams) =>
+    axios.get(`${basePathApplications}${objectToQuery(params)}`).then(r => r.data),
+  updateApplication: (params: IUpdateWorkspaceModelTo) =>
+    axios.patch(`${basePathApplications}/${params.id}/${params.action}`, { ...params })
+};
+export const workspaceTransport = {
+  getWorkspace: (id: string) => axios.get(`${basePathWorkspaces}/${id}`).then(r => r.data),
+  getWorkspaces: (params: IWorkspaceCollectionParams) =>
+    axios.get(`${basePathWorkspaces}${objectToQuery(params)}`).then(r => r.data),
+  updateWorkspace: (params: IUpdateWorkspaceModelTo) =>
+    axios.patch(`${basePathWorkspaces}/${params.id}/${params.action}`, { ...params })
 };
